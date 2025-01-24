6067ecf2de5820c0c4f6aa78b92a6260cb10eabf
박서현 <s24005@gsm.hs.kr>
2025-01-24 23:47:40 +0900

    react router dom

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+
+function Home(){
+  return(
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  );
+}
+
+
+function Topics(){
+  return(
+    <div>
+      <h2>Topics</h2>
+      topics..
+    </div>
+  )
+}
+
+
+
+function Contact(){
+  return(
+    <div>
+      <h2>Contact</h2>
+      contact..
+    </div>
+  )
+}
+
+
+function App(){
+  return(
+    <div>
+      <div>Hello React Router Dom</div>
+      <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/topics'>topics</a></li>
+        <li><a href='/contact'>contact</a></li>
+
+      </ul>
+      <Route path='/'><Home></Home></Route>        
+      <Route path='/topics'><Topics></Topics></Route>
+      <Route path='/contact'><Contact></Contact></Route>
+    </div>
+  );
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
